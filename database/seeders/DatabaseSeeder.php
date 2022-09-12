894f4d087ba9da6be5087d6296f142f5d2e0a0cf
Ruzell <ruzellramirez@DESKTOP-CC0VSVN>
2022-09-12 17:26:52 +0800

    added routes and not found page

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,12 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Counter;
+use App\Models\Customer;
+use App\Models\Invoice;
+use App\Models\InvoiceItem;
+use App\Models\Product;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,11 +20,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        // Counter::factory(1)->create();
+        // Product::factory(5)->create();
+        // Customer::factory(5)->create();
+        // Invoice::factory(5)->create();
+        // InvoiceItem::factory(5)->create();
     }
 }
